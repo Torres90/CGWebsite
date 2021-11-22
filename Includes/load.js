24004e66634904b625f93f25b0c6395532d13599
Letail <43039035+Letail@users.noreply.github.com>
2021-11-22 12:17:34 +0000

    Added a soft paper bk to all elements

--- Includes/load.js
+++ Includes/load.js
@@ -1,19 +1,3 @@
-// $(document).ready(function () {
-
-//     $('#header').load("./Includes/include_header.html");
-//     $('#footer').load("./Includes/include_footer.html");
-//     $('#navbar').load("./Includes/include_navbar.html");
-
-//     // Posts
-//     $('#post4').load("./Posts/2021-11-17-Learning-Algebra.html");
-//     $('#post3').load("./Posts/2021-11-16-Starting-Book-of-Shaders.html");
-//     $('#post2').load("./Posts/2021-11-12-Lecture-1.html");
-//     $('#post1').load("./Posts/2021-11-12-Start.html");
-
-//     $('#loadOtherScripts').load("/loadOtherScripts.html");
-//     $('#loadOtherScriptsCopy').load("/loadOtherScriptsCopy.html");
-// });
-
 let deferreds = [];
 function load(selector, path) {
     const def = new $.Deferred()
@@ -27,6 +11,7 @@ $(document).ready(function () {
     load('#navbar', "./Includes/include_navbar.html");
 
     // Posts
+    load('#post6', "./Posts/2021-11-18-BoS-Fence-Extra-Functions.html");
     load('#post5', "./Posts/2021-11-18-BoS-Fence.html");
     load('#post4', "./Posts/2021-11-17-Learning-Algebra.html");
     load('#post3', "./Posts/2021-11-16-Starting-Book-of-Shaders.html");
